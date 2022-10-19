8f39c8ff606106c423394e87724e5b3e3a2f633b
Ingvar Suvorin <ingvarr2104@gmail.com>
2022-10-19 21:31:46 +0000

    Updated middle.js with assertArraysEqual function

--- middle.js
+++ middle.js
@@ -5,7 +5,6 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
   }
 };
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -18,19 +17,28 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
 const middle = function(arr) {
   const indexOne = Math.floor((arr.length - 1) / 2);
-   if(arr.length <= 2) {
+  if (arr.length <= 2) {
     return [];
-   }
-    if(arr.length % 2 === 0){
-      let indexTwo = indexOne + 1;
-      return [arr[indexOne], arr[indexTwo]];
-    } else {
-      return [arr[indexOne]];
-    }
+  }
+  if (arr.length % 2 === 0) {
+    let indexTwo = indexOne + 1;
+    return [arr[indexOne], arr[indexTwo]];
+  } else {
+    return [arr[indexOne]];
+  }
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual([1, 2, 3], [1, 3, 3]);
 console.log(middle([1, 2, 4]));
