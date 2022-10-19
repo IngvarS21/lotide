659b08b6af2523f42b92650b34a2f49297c0c0a6
Ingvar Suvorin <ingvarr2104@gmail.com>
2022-10-19 21:27:13 +0000

    AssertArraysEqual function with the test case

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertArraysEqual([1, 2, 3], [1, 3, 3]);
