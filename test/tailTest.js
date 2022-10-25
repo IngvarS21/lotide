8b47aa1ebe5a94562dcc8b4d0ddf98b718f18713
Ingvar Suvorin <ingvarr2104@gmail.com>
2022-10-25 20:09:23 +0000

    Mocha/Chai test for tail.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,18 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
 
-assertEqual(tail([5,6,7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(tail([5,6,7]), 5);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("Tests for tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns 5 for [5]", () => {
+    assert.deepEqual(tail(['5']), []);
+  });
+});
