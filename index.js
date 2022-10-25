b6e6b21e1caf6b53bcea9b49d7c30d9c947e3358
Ingvar Suvorin <ingvarr2104@gmail.com>
2022-10-25 22:44:30 +0000

    Formatted with Object Shorthand

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const assertArraysEqual = require('./assertArraysEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
 const findKeyByValue = require('./findKeyByValue'); 
-const letterPosition = require('./letterPosition');
+const letterPositions = require('./letterPositions');
 const without = require('./without');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKeyByValue: findKeyByValue,
-  letterPosition: letterPosition,
-  without: without
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+  countLetters,
+  countOnly,
+  findKeyByValue,
+  letterPositions,
+  without
 };
