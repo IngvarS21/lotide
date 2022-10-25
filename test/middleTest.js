02781dac7d207e7a2c743b79b997169a5e071351
Ingvar Suvorin <ingvarr2104@gmail.com>
2022-10-25 20:09:04 +0000

    Mocha/Chai test for middle.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,16 @@
-const middle = require ('../middle');
-const assertArraysEqual = require ('../assertArraysEqual');
+// const middle = require ('../middle');
+// const assertArraysEqual = require ('../assertArraysEqual');
 
-console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1, 2, 3, 4, 5]));
+
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns 5 for [5]", () => {
+    assert.deepEqual(middle(['5']), []);
+  });
+});
